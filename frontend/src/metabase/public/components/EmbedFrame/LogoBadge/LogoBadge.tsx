5176cf1eb5e1f3c2f98ebd0c57b2d28be741efe8
Manuel Romero de Terreros <manolordeta@gmail.com>
2025-12-26 12:00:24 -0600

    v1.0.3: Powered by badge, favicons, embed branding
    - Replace Powered by Metabase badge with VSDatabase logo
    - Update embed link to verastrata.com
    - Update embedding legal text for VSDatabase
    - Replace favicons with VSDatabase icons

--- frontend/src/metabase/public/components/EmbedFrame/LogoBadge/LogoBadge.tsx
+++ frontend/src/metabase/public/components/EmbedFrame/LogoBadge/LogoBadge.tsx
@@ -6,37 +6,20 @@ import ExternalLink from "metabase/common/components/ExternalLink";
 import EmbedFrameS from "../EmbedFrame.module.css";
 
 import LogoBadgeStyle from "./LogoBadge.module.css";
-import MetabaseLogoWithText from "./metabase_logo_with_text.svg?component";
+import VeraStrataLogo from "./verastrata_logo_with_text.svg?component";
 
 export const LogoBadge = ({ dark }: { dark: boolean }) => {
-  const utmContentValue = `embedded_banner_${encodeURIComponent(
-    getHostAppUrlDomain(),
-  )}`;
-
   return (
     <ExternalLink
       className={cx(EmbedFrameS.LogoBadge, LogoBadgeStyle.metabaseLink, {
         [LogoBadgeStyle.dark]: dark,
         [LogoBadgeStyle.light]: !dark,
       })}
-      href={`https://www.metabase.com/powered-by-metabase?utm_medium=referral&utm_source=product&utm_campaign=powered_by_metabase&utm_content=${utmContentValue}`}
+      href="https://www.verastrata.com"
       target="_blank"
     >
       <span>{t`Powered by`}</span>
-      {/* eslint-disable-next-line no-literal-metabase-strings -- This embedding badge which we don't want to show the whitelabeled name */}
-      <MetabaseLogoWithText height={32} aria-label="Metabase" />
+      <VeraStrataLogo height={32} aria-label="Vera Strata" />
     </ExternalLink>
   );
 };
-
-function getHostAppUrlDomain() {
-  // based on https://stackoverflow.com/questions/3420004/access-parent-url-from-iframe
-  let referrerUrl =
-    parent !== window ? document.referrer : document.location.href;
-
-  // remove trailing slash
-  referrerUrl = referrerUrl.replace(/\/+$/, "");
-
-  // get domain value, based on https://stackoverflow.com/questions/569137/how-to-get-domain-name-from-url
-  return referrerUrl.replace(/.+\/\/|www.|\..+/g, "");
-}
